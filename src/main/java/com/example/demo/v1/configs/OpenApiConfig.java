a3684b3931995b45f4b68969821ff5a9fac395d7
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-11-02 23:09:17 +0200

    [core]: swagger-properties --addition

--- src/main/java/com/example/demo/v1/configs/OpenApiConfig.java
+++ src/main/java/com/example/demo/v1/configs/OpenApiConfig.java
@@ -1,5 +1,4 @@
 package com.example.demo.v1.configs;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.models.Components;
